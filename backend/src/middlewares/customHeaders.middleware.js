0b53fcf4b7fc46605032216a8a9433b58c7d79f4
ashishmisal23 <ashishmisal23@gmail.com>
2026-01-04 00:22:04 +0530

    response headers added in backend

--- backend/src/middlewares/customHeaders.middleware.js
+++ backend/src/middlewares/customHeaders.middleware.js
@@ -3,15 +3,15 @@ const crypto = require('crypto');
 const customHeadersMiddleware = (req, res, next) => {
   console.log('Custom Headers Middleware Invoked');
   console.log('Request Method:', req.method);
-  console.log('Request URL:', req);
   console.log('Incoming Request Headers:', req.headers);
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Client-Source');
 
   if (req.method === 'OPTIONS') {
-    return next();
+    return res.sendStatus(204);
   }
 
   req.meta = {
