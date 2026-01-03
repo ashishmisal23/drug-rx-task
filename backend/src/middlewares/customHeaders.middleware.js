8ceee5ca058b9b31997a27ac27ed2f2d46117649
ashishmisal23 <ashishmisal23@gmail.com>
2026-01-04 00:15:46 +0530

    backend log changes

--- backend/src/middlewares/customHeaders.middleware.js
+++ backend/src/middlewares/customHeaders.middleware.js
@@ -2,6 +2,14 @@ const crypto = require('crypto');
 
 const customHeadersMiddleware = (req, res, next) => {
   console.log('Custom Headers Middleware Invoked');
+  console.log('Request Method:', req.method);
+  console.log('Request URL:', req);
+  console.log('Incoming Request Headers:', req.headers);
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method === 'OPTIONS') {
     return next();
   }
