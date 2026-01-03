cdc5dc655766efc0d4c7bb80001107f3e2785fe4
Ashish Misal <ashish.misal@sumasoft.net>
2026-01-03 18:58:17 +0530

    url modified

--- frontend/src/components/features/ContactForm.tsx
+++ frontend/src/components/features/ContactForm.tsx
@@ -82,7 +82,7 @@ const ContactForm = () => {
     setErrors({});
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://drug-rx-task-backend.vercel.app/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
